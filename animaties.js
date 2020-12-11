ae52e75b980f40a2f635bb2df1be443effeec495
Firas <firassoekhai@hotmail.nl>
2020-12-11 12:30:41 +0100

    video update

--- animaties.js
+++ animaties.js
@@ -1,4 +1,9 @@
 // klik op animeer-knop laat vlakje animeren door class toe te voegen of weg te nemen
 document.querySelector('.animeer-knop').addEventListener('click', (e) => {
-    document.querySelector('.vlakje').classList.toggle('vlakje__naar-rechts')
-})
+    document.querySelector('.vlakje').classList.toggle('vlakje__naar-rechts');
+});
+
+// na enige tijd na het laden van de pagina start de animatie van het vlakje vanzelf
+setTimeout( () => {
+    document.querySelector('.vlakje').classList.toggle('vlakje__naar-rechts');
+}, 2000 )
