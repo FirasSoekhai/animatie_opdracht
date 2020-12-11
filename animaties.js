71de5c635a46294fd29b12e29a8d3cfe8911a3ec
Firas <firassoekhai@hotmail.nl>
2020-12-11 12:44:06 +0100

    filmpje update

--- animaties.js
+++ animaties.js
@@ -7,3 +7,15 @@ document.querySelector('.animeer-knop').addEventListener('click', (e) => {
 setTimeout( () => {
     document.querySelector('.vlakje').classList.toggle('vlakje__naar-rechts');
 }, 2000 )
+
+// een klik op nav__knop laat de nav__link (alle) na elkaar naar binnen schuiven
+// door ze de class nav__link--schuif-in te geven
+const menuKnop      = document.querySelector('.nav__knop');
+const navigatieItems = document.querySelectorAll('.nav__link');
+menuKnop.addEventListener('click', () => {
+    navigatieItems.forEach( (item,index) => {
+        setTimeout( () => {
+            item.classList.toggle('nav__link--schuif-in');
+        } , 100 * index);
+    })
+});
